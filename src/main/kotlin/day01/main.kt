ee4e7d088d9cabadcc21f94ba2c923d625a16451
Rob Purcell <216741+robpurcell@users.noreply.github.com>
2020-12-02 08:55:20 +0000

    Day 01.1
    Crude approach

--- src/main/kotlin/day01/main.kt
+++ src/main/kotlin/day01/main.kt
@@ -1,7 +1,34 @@
 package day01
 
-fun main(args: Array<String>) {
-  println("Hello World!")
+import java.io.File
+
+fun main() {
+    val numbers = mutableListOf(
+        1721,
+        979,
+        366,
+        299,
+        675,
+        1456
+    )
+//    print(readInput())
+    print("Product: ${calc(readInput())}")
 }
 
+private fun calc(numbers: List<Int>, target: Int = 2020): Int {
+    for (n in numbers) {
+        for (m in numbers) {
+            if (n + m == target) {
+                val product = n * m
+                print("n: $n, m: $m\n")
+                return product
+            }
+        }
+    }
+    return 0
+}
 
+fun readInput(path: String = "/Users/rob/Development/source/github/advent-of-code-2020/src/test/resources/day01-input.txt"): List<Int> {
+    val file = File(path)
+    return file.readLines().map {it.toInt()}.sorted()
+}
